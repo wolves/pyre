a87efa9482ee944637249570e86ad7cf7fbf467a
Wolves <cstingl@protonmail.com>
2024-01-09 11:52:15 -0500

    Update root cmd descriptions

--- cmd/root.go
+++ cmd/root.go
@@ -1,6 +1,5 @@
 /*
 Copyright © 2024 Christopher Stingl <cs@wlvs.io>
-
 */
 package cmd
 
@@ -17,13 +16,8 @@ var cfgFile string
 // rootCmd represents the base command when called without any subcommands
 var rootCmd = &cobra.Command{
 	Use:   "pyre",
-	Short: "A brief description of your application",
-	Long: `A longer description that spans multiple lines and likely contains
-examples and usage of using your application. For example:
-
-Cobra is a CLI library for Go that empowers applications.
-This application is a tool to generate the needed files
-to quickly create a Cobra application.`,
+	Short: "A CLI helper for common Sunbird development tasks",
+	Long:  `Pyre is a CLI tool for help with common and/or repetative tasks within the Sunbird developer eco-system.`,
 	// Uncomment the following line if your bare application
 	// has an action associated with it:
 	// Run: func(cmd *cobra.Command, args []string) { },
