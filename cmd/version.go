a377da82d8264987d8d2827b08f7bd604479c100
Wolves <cstingl@protonmail.com>
2024-03-06 10:50:26 -0500

    Version command grabs version from build info

--- cmd/version.go
+++ cmd/version.go
@@ -5,17 +5,65 @@ package cmd
 
 import (
 	"fmt"
+	"runtime/debug"
+	"time"
 
 	"github.com/spf13/cobra"
 )
 
+var PyreVersion string
+
+func getPyreVersion() string {
+	noVersionAvail := "No version info available for this build, run 'pyre help version' for additional info"
+
+	if len(PyreVersion) != 0 {
+		return PyreVersion
+	}
+
+	buildInfo, ok := debug.ReadBuildInfo()
+	if !ok {
+		return noVersionAvail
+	}
+
+	if len(buildInfo.Main.Version) != 0 {
+		if buildInfo.Main.Version != "(devel)" {
+			return buildInfo.Main.Version
+		}
+	}
+
+	var vcsRevision string
+	var vcsTime time.Time
+	for _, setting := range buildInfo.Settings {
+		switch setting.Key {
+		case "vcs.revision":
+			vcsRevision = setting.Value
+		case "vcs.time":
+			vcsTime, _ = time.Parse(time.RFC3339, setting.Value)
+		}
+	}
+
+	if vcsRevision != "" {
+		return fmt.Sprintf("%s, (%s)", vcsRevision, vcsTime)
+	}
+
+	return noVersionAvail
+}
+
 // versionCmd represents the version command
 var versionCmd = &cobra.Command{
 	Use:   "version",
-	Short: "Display current Pyre application version info",
-	Long:  `The version command provides current version information for the Pyre CLI application.`,
+	Short: "Display application version information",
+	Long: `
+The version command provides information about the application's version.
+
+Pyre requires version information to be embedded at compile time.
+For detailed version information, Pyre needs to be built as specified in the README installation instructions.
+If Pyre is built within a version control repository and other version info isn't available,
+the revision hash will be used instead.
+	`,
 	Run: func(cmd *cobra.Command, args []string) {
-		fmt.Println("Pyre CLI version: 0.1.0")
+		version := getPyreVersion()
+		fmt.Printf("Pyre CLI version: %v\n", version)
 	},
 }
 
